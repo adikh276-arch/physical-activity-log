e4191e69f1b15bdacd79eab748ec058ef5e5ea45
Mantra IT Team <it-team@mantracare.com>
2026-03-10 10:22:11 +0530

    Remove header/footer sections and lovable favicon

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,15 +1,11 @@
-import HeroSection from "@/components/HeroSection";
 import TrackActivitySection from "@/components/TrackActivitySection";
-import ExerciseBenefitsSection from "@/components/ExerciseBenefitsSection";
 import LanguageSelector from "@/components/LanguageSelector";
 
 const Index = () => {
   return (
     <main className="min-h-screen bg-background relative">
       <LanguageSelector />
-      <HeroSection />
       <TrackActivitySection />
-      <ExerciseBenefitsSection />
     </main>
   );
 };
